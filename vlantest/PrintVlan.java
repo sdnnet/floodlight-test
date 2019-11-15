cacb04d0456dbb421c9b07b8655616f8a8d25355
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-11-16 00:31:16 +0530

    Print Vlan ID module success

---
+++ vlantest/PrintVlan.java
@@ -0,0 +1,86 @@
+package net.floodlightcontroller.floodlightTest.vlantest;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.learningswitch.LearningSwitch;
+import net.floodlightcontroller.packet.Ethernet;
+
+public class PrintVlan implements IFloodlightModule,IOFMessageListener{
+
+	protected static Logger log = LoggerFactory.getLogger(LearningSwitch.class);
+	protected IFloodlightProviderService floodlightProviderService;
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+		// TODO Auto-generated method stub
+		floodlightProviderService = context.getServiceImpl(IFloodlightProviderService.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		// TODO Auto-generated method stub
+		floodlightProviderService.addOFMessageListener(OFType.PACKET_IN, this);
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "PrintVlan";
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		// TODO Auto-generated method stub
+		OFPacketIn vmsg = (OFPacketIn)msg;
+		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		log.info("VLAN ID {}",eth.getVlanID());
+		return Command.CONTINUE;
+		
+	}
+
+}
