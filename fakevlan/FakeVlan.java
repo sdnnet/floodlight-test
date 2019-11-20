7cf66702b5cb3b856cc7da985e1cec98b232b121
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-11-20 19:07:11 +0530

    just minor change to fake vlan

--- fakevlan/FakeVlan.java
+++ fakevlan/FakeVlan.java
@@ -1,20 +1,35 @@
 package net.floodlightcontroller.floodlightTest.fakevlan;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
-import org.projectfloodlight.openflow.protocol.*;
-import org.projectfloodlight.openflow.protocol.action.*;
-import org.projectfloodlight.openflow.protocol.instruction.*;
-import org.projectfloodlight.openflow.protocol.match.*;
-import org.projectfloodlight.openflow.protocol.oxm.*;
-import org.projectfloodlight.openflow.types.*;
-import org.python.antlr.PythonParser.return_stmt_return;
-import org.slf4j.*;
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.protocol.oxm.OFOxms;
+import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.OFVlanVidMatch;
+import org.projectfloodlight.openflow.types.TableId;
+import org.projectfloodlight.openflow.types.VlanVid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import net.floodlightcontroller.core.*;
-import net.floodlightcontroller.core.module.*;
-import net.floodlightcontroller.packet.*;
-import net.floodlightcontroller.util.FlowModUtils;
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
 
 public class FakeVlan implements IFloodlightModule , IOFMessageListener{
 
@@ -53,10 +68,10 @@ public class FakeVlan implements IFloodlightModule , IOFMessageListener{
 			.setBufferId(OFBufferId.NO_BUFFER)
 			.setHardTimeout(0)
 			.setIdleTimeout(10)
-			.setPriority(3276)
+			.setPriority(1)
 			.setMatch(m)
 			.setActions(al)
-			.setTableId(TableId.of(3))
+			.setTableId(TableId.of(1))
 			.build();
 		// and write it out
 		sw.write(flowAdd);
